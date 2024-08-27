3f9d9432f047acdce5768d9ab934c38f4270d4f8
Abdi Deresa <Abdishegute@gmail.com>
2024-08-27 14:13:57 -0700

    added the food class

--- file.py
+++ file.py
@@ -7,7 +7,7 @@ SPEED = 50
 SPACE_SIZE = 50
 BODY_PARTS = 3
 SNAKE_COLOR = "#00FF00"
-FOOD_COLOR = "FF0000"
+FOOD_COLOR = "#FF0000"
 BACKGROUND_COLOR = "#000000"
 
 
@@ -16,7 +16,14 @@ class Snake:
     pass
 
 class Food:
-    pass
+    def __init__(self):
+        x=random.randint(0,(GAME_WIDTH//SPACE_SIZE)-1)*SPACE_SIZE
+        y=random.randint(0,(GAME_HEIGHT//SPACE_SIZE)-1)*SPACE_SIZE
+        
+        self.coordinates = [x,y]
+        
+        canvas.create_oval(x,y, x+SPACE_SIZE,y+SPACE_SIZE, fill=FOOD_COLOR, tag="food")
+        
 
 def next_turn():
     pass
@@ -57,6 +64,9 @@ x=int((screen_width/2)-(window_width/2))
 y=int((screen_height/2)-(window_height/2))
 
 window.geometry(f"{window_width}x{window_height}+{x}+{y}")
+
+snake = Snake()
+food = Food()
 
 
 
